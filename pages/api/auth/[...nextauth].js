4af54d0e06ddff479c24e4410a0d17337ce9e512
Faysal-Ahamed-Tarek <faysal.ahamed.tarek1@gmail.com>
2021-09-22 07:42:36 -0700

    Auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ export default NextAuth({
 
   providers: [
     GithubProvider({
-      clientId: "05d4e339ca06e8af6bd5",
-      clientSecret: "555fc9cbd345c0d7308f17adea5283a5ee1e25c6",
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   pages: {
